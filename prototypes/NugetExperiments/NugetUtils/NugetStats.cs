c062141be5278a0f05bb36b55a203834a2ce0c67
Jan Krivanek <jankrivanek@microsoft.com>
2023-03-03 18:55:05 +0100

    Initial commit

---
+++ prototypes/NugetExperiments/NugetUtils/NugetStats.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+
+namespace NugetUtils
+{
+    public class NugetStats
+    {
+        private const string nugetStatsUrl = @"https://www.nuget.org/stats/packages";
+        private HttpClient _client = new HttpClient();
+
+        public async Task AsyncMethod()
+        {
+            using HttpResponseMessage response = await _client.GetAsync(nugetStatsUrl);
+            using HttpContent content = response.Content;
+            string res = await content.ReadAsStringAsync();
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(res);
+            var tables = document.DocumentNode.Descendants("table").ToList();
+
+            //data-bind="visible: showAllPackageDownloads"
+            //data-bind="visible: !showAllPackageDownloads()"
+            //aria-label="Community packages with the most downloads"
+
+            HtmlNode communityTable = tables.First(t =>
+                t.Attributes.Any(a => a.Value.Contains("community", StringComparison.CurrentCultureIgnoreCase)));
+
+            HtmlNode allTable = tables.First(t => !object.ReferenceEquals(t, communityTable));
+        }
+
+        private void ParseTable(HtmlNode table)
+        {
+            //tbd
+        }
+    }
+}
---
+++ prototypes/NugetExperiments/Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using NugetUtils;
+
+namespace Runner
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            await new NugetStats().AsyncMethod();
+        }
+    }
+}
